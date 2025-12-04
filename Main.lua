91d65331f4f069cd143f3d793b9ded40e408ce93
elony-7 <elony7@outlook.com>
2025-12-04 16:48:08 +0800

    Update

--- Main.lua
+++ Main.lua
@@ -20,13 +20,9 @@ Notify("Notification", "Script is loading...", 5)
 -- Preload Modules
 --========================
 local AutoFishing =
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/elony-7/RBXAFSH/main/Module/autofishingtest.lua"))()
-local TeleportToPlayer =
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/elony-7/RBXAFSH/main/Module/Teleporttoplayer.lua"))()
-local PlayerModule =
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/elony-7/RBXAFSH/main/Module/PlayerModule.lua"))()
+	c
 local antiafkmodule =
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/elony-7/RBXAFSH/main/Module/Antiafk.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/elony-7/PublicIseng/main/AntiAFKGabut.lua"))()
 
 print("Modules loaded:", AutoFishing, TeleportToPlayer, PlayerModule, antiafkmodule)
 
@@ -48,11 +44,6 @@ print("Created Window:", Window)
 -- Create Tabs
 --========================
 local FarmTab = Window:AddTab({ Title = "Farm", Icon = "play" })
---local TeleportTab = Window:AddTab({Title = "Teleport", Icon = "map"})
-local TeleportPlayerTab = Window:AddTab({ Title = "Teleport to Player", Icon = "user" })
-local PlayerTab = Window:AddTab({ Title = "Player", Icon = "user-cog" })
---local AutoSellTab = Window:AddTab({Title = "Auto Sell", Icon = "shopping-cart"})
---local WeatherTab = Window:AddTab({Title = "Weather", Icon = "cloud-rain"})
 local ExtraTab = Window:AddTab({ Title = "Extra", Icon = "settings" })
 
 local Options = Fluent.Options
@@ -78,42 +69,6 @@ do
 			Notify("❌ Auto Fishing Perfect", "Auto Fishing Perfect DISABLED", 2)
 		end
 	end)
-end
-
---========================
--- Teleport to Player Tab
---========================
-do
-	local playerList = TeleportToPlayer.GetInitialPlayers() or {}
-	if #playerList == 0 then
-		table.insert(playerList, "None")
-	end
-	local selectedPlayer = playerList[1]
-
-	TeleportToPlayer.selectedPlayerName = selectedPlayer
-
-	local playerDropdown = TeleportPlayerTab:AddDropdown("SelectPlayerDropdown", {
-		Title = "Select Player",
-		Values = playerList,
-		Multi = false,
-		Default = selectedPlayer,
-	})
-
-	playerDropdown:SetValue(selectedPlayer)
-	playerDropdown:OnChanged(function(value)
-		selectedPlayer = value
-		TeleportToPlayer.selectedPlayerName = value
-		print("Selected player:", value)
-	end)
-
-	TeleportPlayerTab:AddButton({
-		Title = "Teleport to Player",
-		Description = "Teleport to the selected player",
-		Callback = function()
-			TeleportToPlayer.TeleportTo(selectedPlayer)
-			Notify("Teleport", "Teleported to " .. selectedPlayer, 2)
-		end,
-	})
 end
 
 --========================
